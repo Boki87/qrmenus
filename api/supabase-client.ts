ad23a8a33258c5b6d3cbb2bd93d1e7e7d226d2f7
Boki87 <peric.bojan87@gmail.com>
2022-10-11 14:56:35 +0200

    fixed build bugs

--- api/supabase-client.ts
+++ api/supabase-client.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabase_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
+console.log(supabase_url)
 const supabase = createClient(supabase_url ? supabase_url : '', supabase_key ? supabase_key : '')
 
 export {
