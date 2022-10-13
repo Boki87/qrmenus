9b74831c69a1cdd5fcf607dc123c69f3c47c0c32
Boki87 <peric.bojan87@gmail.com>
2022-10-13 10:12:14 +0200

    fixed build bugs

--- api/supabase-client.ts
+++ api/supabase-client.ts
@@ -1,18 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 
-const APP_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://qrmenus-nu.vercel.app'
+const APP_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://qrmenus-nu.vercel.app";
 
-//const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL
-//const supabase_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabase = createClient(
+  "https://hvuxclagvnwtryxuskjy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dXhjbGFndm53dHJ5eHVza2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQxODcyNzcsImV4cCI6MTk3OTc2MzI3N30.rS3ATJA8rA0kfNcHHNn7Hg7I_3zOwIpdiK5xIpLvo0w"
+);
 
-//const supabase = createClient("https://hvuxclagvnwtryxuskjy.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dXhjbGFndm53dHJ5eHVza2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQxODcyNzcsImV4cCI6MTk3OTc2MzI3N30.rS3ATJA8rA0kfNcHHNn7Hg7I_3zOwIpdiK5xIpLvo0w")
-
-const supabase_url = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabase_key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-const supabase = createClient(supabase_url,supabase_key)
-
-export {
-    supabase,
-    APP_URL
-}
+export { supabase, APP_URL };
