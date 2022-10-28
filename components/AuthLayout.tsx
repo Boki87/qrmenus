00febf2042219c713dcfd7c938d38aec18a9b3b6
Boki87 <peric.bojan87@gmail.com>
2022-10-28 12:37:35 +0200

    added bg to auth pages

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,23 +1,33 @@
-import {Box, useColorModeValue} from "@chakra-ui/react"
-import { ReactNode } from "react"
-
+import { Box, useColorModeValue, Image } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface AuthLayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
-const AuthLayout = ({children}: AuthLayoutProps) => {
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return (
+    <Box w="full" h="full" display="flex">
+      <Box flex="1" bg={useColorModeValue("white", "gray.700")} px="20px">
+        {children}
+      </Box>
+      <Box
+        flex="1"
+        bg={useColorModeValue("gray.100", "gray.900")}
+        display={{ base: "none", xl: "flex" }}
+        alignItems="center"
+        justifyContent="center"
+      >
+        {/*Your bg here*/}
+        <Image
+          src="/images/auth-bg.jpg"
+          minW="full"
+          minH="full"
+          objectFit="cover"
+        />
+      </Box>
+    </Box>
+  );
+};
 
-
-    return (<Box w="full" h="full" display="flex">
-        <Box flex="1" bg={useColorModeValue('white', 'gray.700')} px="20px">
-            {children}
-        </Box>
-        <Box flex="1" bg={useColorModeValue('gray.100', 'gray.900')} display={{base:'none', xl:'block'}}>
-            {/*Your bg here*/}
-        </Box>
-        </Box>
-    )
-}
-
-export default AuthLayout
+export default AuthLayout;
